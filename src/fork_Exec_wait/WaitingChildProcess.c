689cf8103b817cd34ac4479f44a96e2e7110faf4
Rafii-Ayoub <72503025+Rafii-Ayoub@users.noreply.github.com>
2021-06-04 20:20:03 +0200

    Create WaitingChildProcess.c

---
+++ src/fork_Exec_wait/WaitingChildProcess.c
@@ -0,0 +1,47 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h> 
+#include <stdlib.h> 
+#include <sys/wait.h>
+#define MAX 10
+  
+int main()
+{
+  
+  int fd[2], i = 0;
+  pipe(fd);
+  pid_t pid = fork();
+  
+   if(pid > 0) {
+      wait(NULL);
+  
+      close(0);
+      close(fd[1]); 
+  
+
+      dup(fd[0]); 
+      int arr[MAX];
+ 
+      int n = read(fd[0], arr, sizeof(arr));
+      for ( i = 0;i < n/4; i++)
+  
+         // printing the array received from child process
+          printf("%d ", arr[i]); 
+  } 
+  else if( pid == 0 ) {
+      int arr[] = {1, 2, 3, 4, 5};
+  
+      
+      close(fd[0]); 
+      close(1);    
+  
+      // duplicating fd[0] with standard output 1
+      dup(fd[1]);  
+      write(1, arr, sizeof(arr));
+  } 
+  
+  else {
+      perror("error\n"); //fork()
+  }
+} 
