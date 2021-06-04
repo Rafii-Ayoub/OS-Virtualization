4f370e76c6adccf0ec93af952a65aaa58bf6a9c2
Rafii-Ayoub <72503025+Rafii-Ayoub@users.noreply.github.com>
2021-06-04 20:04:52 +0200

    Update Zombie.c

--- src/fork_Exec_wait/Zombie.c
+++ src/fork_Exec_wait/Zombie.c
@@ -2,13 +2,13 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-// this program help us to observe a Zombie.
+// this program help us to observe a Zombie process.
 
 int main ()
 {
 pid_t child_pid;child_pid = fork ();
 if (child_pid > 0) {
-sleep (60);
+sleep (10);
 }
 else {
 exit (0);
